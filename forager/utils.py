e4b03be7b6504aacaba11a08234676684f0cb714
Freddy Snijder <freddy@visionscapers.com>
2024-12-29 00:20:45 +0100

    Added utility order files in 'natural' order

--- forager/utils.py
+++ forager/utils.py
@@ -1,6 +1,9 @@
+from typing import List
+
 import os
 import glob
-from typing import List
+
+from natsort import natsorted, ns
 
 
 def find_files_recursive(input_path: str, extension_patterns: List[str]) -> List[str]:
@@ -9,3 +12,7 @@ def find_files_recursive(input_path: str, extension_patterns: List[str]) -> List
         file_paths += list(glob.glob(os.path.join(input_path, "**", ext_pattern), recursive=True))
 
     return file_paths
+
+
+def natural_sort(paths: List[str]) -> List[str]:
+    return natsorted(paths, alg=ns.IGNORECASE)

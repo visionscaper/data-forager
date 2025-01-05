f9cb43b843a03be9a244f1ee7d77fd33573da5c5
Freddy Snijder <freddy@visionscapers.com>
2025-01-05 13:17:14 +0100

    Created TokensDataset that can use the indexing data of a FileTextLinesIndexern that used a TokenizedSampleGenerator

---
+++ forager/datasets/tokens.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+import numpy as np
+
+from forager.sample_index import SampleIndex
+from forager.datasets.common import Dataset
+
+
+class TokensDataset(Dataset):
+
+    def __init__(
+            self,
+            sample_index: SampleIndex,
+            token_dtype: np.uint16,
+            name: Optional[str] = None,
+            **kwargs,
+    ):
+        super().__init__(sample_index, name=name, **kwargs)
+
+        self._token_dtype = token_dtype
+
+    def _process_sample(self, sample_bytes: bytes) -> np.ndarray:
+        return np.frombuffer(sample_bytes, dtype=self._token_dtype)
